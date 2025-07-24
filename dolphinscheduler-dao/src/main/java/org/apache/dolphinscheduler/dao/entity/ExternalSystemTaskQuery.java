ba87e209f05a4d4c799f4c7be65f11ba4e7e2807
yud8 <yud8@chinatelecom.cn>
2025-07-24 13:53:15 +0800

    BDPL-32519 新增外部系统接口2

---
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/ExternalSystemTaskQuery.java
@@ -0,0 +1,14 @@
+package org.apache.dolphinscheduler.dao.entity;
+
+
+import lombok.Data;
+
+@Data
+public class ExternalSystemTaskQuery {
+
+    private String id;
+
+    private String name;
+
+
+}
