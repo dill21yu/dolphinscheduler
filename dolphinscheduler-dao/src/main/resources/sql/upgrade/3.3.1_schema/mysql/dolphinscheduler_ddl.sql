04b8e3d413a94d9215afdfa7b0e05cccd53249bf
yud8 <yud8@chinatelecom.cn>
2025-08-28 09:22:40 +0800

    BDPL-32519 mysql update 3.3.1

--- dolphinscheduler-dao/src/main/resources/sql/upgrade/3.3.1_schema/mysql/dolphinscheduler_ddl.sql
+++ dolphinscheduler-dao/src/main/resources/sql/upgrade/3.3.1_schema/mysql/dolphinscheduler_ddl.sql
@@ -18,3 +18,28 @@
 ALTER TABLE t_ds_workflow_definition DROP PRIMARY KEY;
 ALTER TABLE t_ds_workflow_definition ADD PRIMARY KEY(id);
 ALTER TABLE t_ds_workflow_definition ADD UNIQUE KEY uniq_workflow_definition_code (code);
+
+
+DROP TABLE IF EXISTS `t_ds_external_system`;
+CREATE TABLE `t_ds_external_system` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary key',
+  `name` VARCHAR(255) COLLATE utf8_bin NOT NULL COMMENT 'System name',
+  `type` VARCHAR(50) COLLATE utf8_bin COMMENT '[Temporarily unused] System type',
+  `connection_params` TEXT COLLATE utf8_bin NOT NULL COMMENT 'Connection parameters (JSON format)',
+  `user_id` INT(11) NOT NULL COMMENT 'Creator user ID',
+  `create_time` DATETIME NOT NULL COMMENT 'Creation time',
+  `update_time` DATETIME NOT NULL COMMENT 'Update time',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_name` (`name`)
+) ENGINE=INNODB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+
+DROP TABLE IF EXISTS `t_ds_relation_external_system_user`;
+CREATE TABLE `t_ds_relation_external_system_user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary key',
+  `user_id` INT(11) NOT NULL COMMENT 'User ID',
+  `external_system_id` INT(11) NOT NULL COMMENT 'External system ID',
+  `perm` INT(11) DEFAULT '1' COMMENT 'Permission level',
+  `create_time` DATETIME DEFAULT NULL COMMENT 'Creation time',
+  `update_time` DATETIME DEFAULT NULL COMMENT 'Update time',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
